f7693ba5e0b8354bcda23cf23fd868a0bb8d51e1
Julian M Bucknall <julianb@boyet.com>
2017-01-04 18:09:06 -0700

    Initial code commit
    Reverted to supporting Windows Live Writer with this project, since have
    now created an Open Live Writer project for the same functionality.

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# WLWSpecialTags
-Windows Live Writer: convert special text markup into HTML entities
+﻿Windows Live Writer special tag/entity inserter
+===
+
+_Inserting code elements and entity characters into Windows Live Writer_
+
+Introduction
+---
+
+The blog posts I write are mostly about code and the like. I found myself wishing for the ability to insert code identifiers into the main body of the document like this: `someIdentifier`. Yes, I could go into the Source view and manually add them, but I wanted to be able to type them markdown style: \`someIdentifer\`, press a couple of keystrokes and magically get it done.
+
+Then, along the same lines, I wanted to be able to insert special entity characters into the text. Things like non-breaking spaces, the multiply character, trademarks, and so on. 
+
+Enter the Windows Live Writer special tag/entity inserter.
+
+Using with Window Live Writer
+===
+
+Compiling the code with Visual Studio will produce a DLL called `WLWSpecialTags.dll`. Make sure you are not running Windows Live Writer, then copy this DLL to the `\Program Files (x86)\Windows Live\Writer\Plugins` folder (you'll need admin rights). Restart Window Live Writer. 
+
+To convert some text, select it, go to the Insert tab, and look in the Plug-ins folder. Select Special Tag Inserter and the selected text will be converted.
+
+Conversions made
+===
+
+These are the conversions made by the plug-in:
+
+- `` `selectedtext` `` is converted to `<code>selectedtext</code>`
+- `?` is converted to a complete list of the entities known by the plug-in
+- if the selected text is not one of the abbreviations below, it is converted as `&selectedtext;`
+
+This is the list of known entity abbreviations, plus the converted character
+
+- `sp` => `&nbsp;`   non-breaking space
+- `>>` => `&#xbb;`   chevron right
+- `<<` => `&#xab;`   chevron left
+- `&gt;&gt;` => `&#xbb;`   chevron right
+- `&lt;&lt;` => `&#xab;`   chevron left
+- `(c)` => `&#xa9;`   copyright
+- `S` => `&#xa7;`   section mark
+- `P` => `&para;`   paragraph mark
+- `*` => `&#x2022;`   bullet
+- `.` => `&#xb7;`   middle dot 
+- `-` => `&#x2013;`   dash
+- `--` => `&#x2014;`   long dash
+- `:>` => `&#x25b6;`   large triangle pointing right
+- `:&gt;` => `&#x25b6;`   large triangle pointing right
+- `o` => `&#x25e6;`   degree symbol
+- `[]` => `&#x25ab;`   small middle square
+- `<>` => `&#x25c7;`   diamond
+- `&lt;&gt;` => `&#x25c7;`   diamond
+- `1/2` => `&frac12;`   1/2 as character
+- `1/4` => `&frac14;`   1/4 as character
+- `3/4` => `&frac34;`   3/4 as character
+- `tm` => `&trade;`   trademark
+- `(r)` => `&reg;`   registered trademark
+- `...` => `&hellip;`   ellipsis
+- `x` => `&times;`   multiply operator
+- `/` => `&divide;`   divide operator
+
+So for example, selecting the text `:>` and invoking the plug-in will replace the selected text with &#x25b6;.
+
---
+++ WLWSpecialTags/SpecialTagInserter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using WindowsLive.Writer.Api;
+
+namespace WLWSpecialTags {
+
+  [WriterPlugin("B8D71B52-C57C-425c-A2DC-37B9F08305C4", "Special Tag Inserter")]
+  [InsertableContentSource("Special Tag/Entity")]
+  public class SpecialTagInserter : ContentSource {
+
+    Dictionary<string, string> EntityMap;
+
+    public SpecialTagInserter()
+      : base() {
+      CreateEntityMap();
+    }
+    public override DialogResult CreateContent(IWin32Window dialogOwner, ref string content) {
+
+      // The different types of content and how they're converted:
+      // `something` => <code>something</code>
+      // something => an entity character, given by the EntityMap below
+      // otherwise, just returns the content wrapped in "&" and ";"
+
+      string trimmedContent = "";
+      DialogResult result = DialogResult.Cancel;
+
+      if (HasContent(content, ref trimmedContent)) { 
+        if (IsValidMarkdownCode(trimmedContent)) {
+          content = Embed(GetMiddle(trimmedContent), "code");
+        }
+        else {
+          content = ConvertEntity(trimmedContent);
+        }
+        result = DialogResult.OK;
+      }
+      return result;
+    }
+
+    private static bool IsFirstChar(string s, char c) {
+      return s[0] == c;
+    }
+    private static bool IsLastChar(string s, char c) {
+      return s[s.Length - 1] == c;
+    }
+    private static string GetMiddle(string s) {
+      return s.Substring(1, s.Length - 2);
+    }
+    private static bool IsValidMarkdownCode(string s) {
+      return (s.Length >= 3) && IsFirstChar(s, '`') && IsLastChar(s, '`');
+    }
+    private static bool HasContent(string s, ref string trimmed) {
+      if (string.IsNullOrEmpty(s)) {
+        return false;
+      }
+      else {
+        trimmed = s.Trim();
+        return !string.IsNullOrEmpty(trimmed);
+      }
+    }
+    public static string Embed(string s, string tag) {
+      return String.Format("<{0}>{1}</{0}>", tag, s);
+    }
+    public string ConvertEntity(string s) {
+      if (s == "?")
+        return DumpEntityMap();
+      if (EntityMap.ContainsKey(s))
+    	  return EntityMap[s];
+      return String.Format("&{0};", s);
+    }
+    public string DumpEntityMap() {
+      string result = "Entity map:<br />";
+      foreach (KeyValuePair<string, string> item in EntityMap) {
+        result += String.Format("[{0}] => [{1}]<br />", item.Key, item.Value);
+      }
+      return result;
+    }
+
+    public void CreateEntityMap() {
+      EntityMap = new Dictionary<string, string>();
+      EntityMap.Add("sp","&nbsp;");         // non-breaking space
+      EntityMap.Add(">>", "&#xbb;");        // chevron right
+      EntityMap.Add("<<", "&#xab;");        // chevron left
+      EntityMap.Add("&gt;&gt;", "&#xbb;");  // chevron right
+      EntityMap.Add("&lt;&lt;", "&#xab;");  // chevron left
+      EntityMap.Add("(c)", "&#xa9;");       // copyright
+      EntityMap.Add("S", "&#xa7;");         // section mark
+      EntityMap.Add("P", "&para;");         // paragraph mark
+      EntityMap.Add("*", "&#x2022;");       // bullet
+      EntityMap.Add(".", "&#xb7;");         // middle dot 
+      EntityMap.Add("-", "&#x2013;");       // dash
+      EntityMap.Add("--", "&#x2014;");      // long dash
+      EntityMap.Add(":>", "&#x25b6;");      // large triangle pointing right
+      EntityMap.Add(":&gt;", "&#x25b6;");   // large triangle pointing right
+      EntityMap.Add("o", "&#x25e6;");       // degree symbol
+      EntityMap.Add("[]", "&#x25ab;");      // small middle square
+      EntityMap.Add("<>", "&#x25c7;");      // diamond
+      EntityMap.Add("&lt;&gt;", "&#x25c7;");// diamond
+      EntityMap.Add("1/2", "&frac12;");     // 1/2 as character
+      EntityMap.Add("1/4", "&frac14;");     // 1/4 as character
+      EntityMap.Add("3/4", "&frac34;");     // 3/4 as character
+      EntityMap.Add("tm", "&trade;");       // trademark
+      EntityMap.Add("(r)", "&reg;");        // registered trademark
+      EntityMap.Add("...", "&hellip;");     // ellipsis
+      EntityMap.Add("x", "&times;");        // multiply operator
+      EntityMap.Add("/", "&divide;");       // divide operator
+    }
+  }
+}
